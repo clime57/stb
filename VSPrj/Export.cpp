80f826d5fc2551908bf74c8c4fa6d3ca3dd4867e
qingshui <36473669@qq.com>
2019-04-04 12:44:12 +0800

    windows and uwparm64 projects

---
+++ VSPrj/Export.cpp
@@ -0,0 +1,19 @@
+﻿#define STB_IMAGE_IMPLEMENTATION
+#include "../stb_image.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	__declspec(dllexport) void* loadFromMemory(stbi_uc const * buffer, int len, int& x, int& y, int& channelsInFile, int desiredChannels)
+	{
+		return stbi_load_from_memory(buffer, len, &x, &y, &channelsInFile, desiredChannels);
+	}
+
+	__declspec(dllexport) void imageFree(void* buffer)
+	{
+		stbi_image_free(buffer);
+	}
+
+#ifdef __cplusplus
+}
+#endif
